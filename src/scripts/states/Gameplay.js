f088e95393bef96607433734e275f609bd8672c3
Gabriel Edwards <gabrieledwards@ymail.com>
2017-04-22 17:13:04 -0700

    Fixed object drag and launch. Line is leftover though

--- src/scripts/states/Gameplay.js
+++ src/scripts/states/Gameplay.js
@@ -6,26 +6,52 @@ export default class Gameplay extends _State {
   create () {
     game.physics.startSystem(Phaser.Physics.P2JS); //Starting the p2 physics
     this.stage.backgroundColor = '#223344';
-game.physics.p2.restitution = 0.8;    this.world.setBounds(0, 0, 320, 288);
+    game.physics.p2.restitution = 0.8;
+    this.world.setBounds(0, 0, 320, 288);
     this.player = GameObjects.player(game, this.world.centerX, 60);
+    this.drawLine = false;
     this.ThrowableObject = GameObjects.throwable(game, this.world.centerx, this.world.centery); //Importing the ThrowableObject
     this.MouseObject = GameObjects.mouse(game, game.input.x, game.input.y);
     this.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON);
+    this.line = new Phaser.Line(this.MouseObject.body.x, this.MouseObject.body.y, this.ThrowableObject.body.x, this.ThrowableObject.body.y);
 
 
     this.add.existing(this.titleText());
     this.add.existing(this.player);
     this.add.existing(this.ThrowableObject);  //Adding the throwable object
     this.add.existing(this.MouseObject);
+    //this.MouseObject = static;
+    game.input.onDown.add(this.click, this);
     game.input.addMoveCallback(this.move, this);
-
+    game.input.onUp.add(this.release, this);
  }
  move(pointer, x, y, isDown) {
-    console.log("X:" + this.MouseObject.x );
-    console.log("Y:"+ this.MouseObject.y);
-    this.MouseObject.x = x;
-    this.MouseObject.y = y;
+    this.MouseObject.body.x = x;
+    this.MouseObject.body.y = y;
+    //this.line.setTo(this.MouseObject.body.x, this.MouseObject.body.y, this.ThrowableObject.body.x, this.ThrowableObject.body.y)
+    //this.drawLine = true;
   }
+
+  click(pointer) {
+
+    var bodies = game.physics.p2.hitTest(pointer.position, [ this.ThrowableObject ]);
+
+    if (bodies.length)
+    {
+        //  Attach to the first body the mouse hit
+        this.mouseSpring = game.physics.p2.createSpring(this.MouseObject, bodies[0], 0, 30, 1);
+        this.line.setTo(this.MouseObject.body.x, this.MouseObject.body.y, this.ThrowableObject.body.x, this.ThrowableObject.body.y);
+        //line.setTo(cow.x, cow.y, mouseBody.x, mouseBody.y);
+        this.drawLine = true;
+    }
+
+}
+  release() {
+
+    game.physics.p2.removeSpring(this.mouseSpring);
+    this.drawLine = false;
+  }
+
   titleText () {
     return DisplayObjects.displayFont(game, 'THIS IS THE GAME', this.world.centerX, 40, 'center');
   }
@@ -47,4 +73,23 @@ game.physics.p2.restitution = 0.8;    this.world.setBounds(0, 0, 320, 288);
       this.player.normal();
     }
   }
+
+  preRender() {
+
+    if (this.line)
+    {
+        this.line.setTo(this.MouseObject.body.x, this.MouseObject.body.y, this.ThrowableObject.body.x, this.ThrowableObject.body.y);
+    }
+
+}
+ render() {
+
+    if (this.drawLine)
+    {
+        game.debug.geom(this.line);
+    console.log("Game.debug");
+  }else
+    console.log("render");
+
+ }
 }
